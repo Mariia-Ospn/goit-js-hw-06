6caca71ac02e2ca53ab4948945b2e3c07bebdecf
Mariia-Ospn <126498309+Mariia-Ospn@users.noreply.github.com>
2023-07-03 18:26:58 +0200

    task 7,8 fixed

--- js/task-07.js
+++ js/task-07.js
@@ -2,6 +2,7 @@ const refs = {
   input: document.querySelector("#font-size-control"),
   text: document.querySelector("#text"),
 };
+refs.input.value = 16;
 
 const handelInput = (event) => {
   refs.text.style.fontSize = `${refs.input.value}px`;
